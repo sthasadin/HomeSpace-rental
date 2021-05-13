87d1073ef0a47188ee8505d429599c545099ebda
sthasadin <shrestha.sadin007@gmail.com>
2021-05-13 20:58:41 +0530

    added the error page and the banner with btn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner'
 
 export default function Home() {
   return (
-    <Hero />
+    <Hero>
+      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at Rs.1999">
+        <Link to='/rooms' className="btn-primary">Rooms</Link>
+      </Banner>
+    </Hero>
   )
 }
