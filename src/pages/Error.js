995a88ad0b719967ae36c8f04ed0a2d28900cd2b
sthasadin <shrestha.sadin007@gmail.com>
2021-05-13 21:17:34 +0530

    added all banners

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import Hero from '../components/Hero'
+import {Link} from 'react-router-dom';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+
 
 const Error = () => {
   return (
-    <Hero/>
+    <Hero>
+      <Banner title="404" subtitle="Page Not Found" >
+        <Link to='/' className='btn-primary'>Go to Home</Link>
+      </Banner>
+    </Hero>
   )
 }
 
